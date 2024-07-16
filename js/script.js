110f346751e66c614a5f3aeb6c1b9b4de59223ad
Norbert Śleziak <norr.bitt@gmail.com>
2024-07-16 23:55:07 +0200

    Delete comments

--- js/script.js
+++ js/script.js
@@ -1,41 +1,10 @@
-/*
-Configuration
-------------------------
-If something doesn't work please contact me on discord (Astronawta#0012).
-*/
-
 const config = {
     serverInfo: {
-        serverLogoImageFileName: "logo.png", /*This is a file name for logo in /images/ (If you upload new logo with other name, you must change this value)*/
-        serverName: "GlobMc.pl", /*Server name*/
-        serverIp: "globmc.pl", /*Server IP (if you want to add online user counter, you must have true the enable-status and enable-query of server.properties)*/
-        discordServerID: "489529070913060867" /*Your server ID (if you want to add online user counter, you must have enabled Discord server widget)*/
+        serverLogoImageFileName: "logo.png", 
+        serverName: "GlobMc.pl", 
+        serverIp: "globmc.pl", 
+        discordServerID: "489529070913060867" 
     },
-
-    /*Admin-Team
-    ------------
-    If you want to create new group, you must add this structure to adminTeamPage:
-    <nameOfGroup>: [
-        {
-            inGameName: "Astronavta",
-            rank: "Owner",
-            skinUrlOrPathToFile: "",
-            rankColor: ""
-        },
-    ]
-    then you must add this group with same name to atGroupsDefaultColors and set the color you want for the group.
-    You can also set a special color for a specific user, just put it in the rankColor of that user.
-
-    All skins for original players are generate automaticaly. If you want to add skins to warez players, yout must add url for skin to skinUrlOrPathToFile
-        {
-            inGameName: "Astronavta",  <--- In-Game name
-            rank: "Owner",  <-- rank
-            skinUrlOrPathToFile: "",  <-- url or file path for skin image for warez players (if you have original minecraft leave it be empty)
-            rankColor: "rgba(255, 3, 3, 1)"  <-- special rank color
-        },
-
-    If you want to change skin type replace userSKinTypeInAdminTeam with something you want from array in comments
-    */
     userSKinTypeInAdminTeam: "bust", /*[full, bust, head, face, front, frontFull, skin]*/
     atGroupsDefaultColors: {
         leaders: "rgba(255, 124, 124, 0.5)",
@@ -89,22 +58,6 @@ const config = {
 
 /*If you want to change website color go to /css/global.css and in :root {} is a color pallete (don't change names of variables, change only values)*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*If you want everything to work as it should and you don't understand what is written here, don't touch it :D*/
 
 
 /*Mobile navbar (open, close)*/
