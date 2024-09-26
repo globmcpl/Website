796d50e10fa3e7682cc15bbf0f1203f3bc54bb06
unknown <kacpersleziak00@gmail.com>
2024-09-26 12:07:00 +0200

    Adjusted rank table to new type of config

--- js/loadRanks.js
+++ js/loadRanks.js
@@ -1,52 +1,68 @@
+const createHeaders = (data, table) => {
+  let thead = '<thead><tr><th scope="col">Prefiks</th>';
+  data.forEach(rank => {
+    thead += `<th scope="col"><p style="display: inline;">[</p><p style="display: inline; color: ${rank.color};">${rank.name}</p><p style="display: inline; color: gray;">]</p></th>`;
+  })
+  thead += '</tr></thead>';
+  table.innerHTML = thead;
+}
+
+const createBody = (data, table) => {
+  let tbody = '<tbody>';
+  
+  tbody = createGroups(tbody, data)
+
+  tbody += '</tbody>';
+  table.innerHTML += tbody;
+}
+
+const createGroups = (tbody, data) => {
+  var group_id = 0
+
+  data[0].groups.forEach(group => {
+    tbody += `<tr><th scope="row">${group.name}</th>`;
+
+    const ranks_len = data.length
+
+    for (let i = 0; i < ranks_len; i++) {
+      tbody += `<td></td>`;;
+    }
+    
+    tbody += '</tr>';
+
+    attributes = group.attributes
+    for (attribute in attributes) {
+      tbody = createAttributeRow(data, tbody, attribute, group_id)
+    }
+    group_id += 1
+  })
+
+  return tbody
+}
+
+const  createAttributeRow = (data, tbody, attribute, group_id) => {
+  tbody += `<tr><th scope="row">${attribute}</th>`;
+  
+  data.forEach(rank => { 
+    value = rank.groups[group_id].attributes[attribute]
+
+    if (typeof value === 'boolean') {
+      value = value ? '<i class="fas fa-check"></i>' : '<i class="fas fa-times"></i>';
+    }
+
+    tbody += `<td>${value}</td>`;
+  })
+
+  return tbody
+}
+
 fetch('config/ranks.json')
   .then(response => response.json())
   .then(data => {
     const table = document.getElementById('ranks-table');
-    
-    // Create table header
-    let thead = '<thead><tr><th scope="col">Prefiks</th>';
-    data.ranks.forEach(rank => {
-      thead += `<th scope="col"><p style="display: inline;">[</p><p style="display: inline; color: ${rank.color};">${rank.name}</p><p style="display: inline; color: gray;">]</p></th>`;
-    });
-    thead += '</tr></thead>';
-    table.innerHTML = thead;
 
-    // Create table body
-    let tbody = '<tbody>';
-
-    const attributes = [
-      { key: 'validity', label: 'Ważność' },
-      { key: 'price', label: 'Cena' },
-      { key: 'expMultiplier', label: 'Mnożnik Exp`a' },
-      { key: 'goCoinsMultiplier', label: 'Mnożnik GoCoins' },
-      { key: 'extraKeys', label: 'Dodatkowe klucze przy zakupie rangi' },
-      { key: 'weeklyKeys', label: 'Cotygodniowe darmowe klucze' },
-      { key: 'slotReservation', label: 'Rezerwacja slota' },
-      { key: 'visibility', label: 'Stała widoczność u innych graczy' },
-      { key: 'welcomeMessage', label: 'Wiadomość powitalna' },
-      { key: 'lobbyFly', label: 'Latanie na lobby' },
-      { key: 'createClan', label: 'Możliwość stworzenia klanu' },
-      { key: 'incognito', label: 'Incognito' },
-      { key: 'earlyArenaStart', label: 'Możliwość wczesnego zaczęcia areny' },
-      { key: 'freeBattlePass', label: 'Darmowy karnet bojowy' },
-      { key: 'messageCooldown', label: 'Cooldown pisania wiadomości' },
-      { key: 'freeLegendaryCosmetics', label: 'Darmowe legendarne kosmetyki' },
-      { key: 'freeEpicCosmetics', label: 'Darmowe epickie kosmetyki' },
-      { key: 'freeRareCosmetics', label: 'Darmowe rzadkie kosmetyki' },
-      { key: 'freeCommonCosmetics', label: 'Darmowe zwyczajne kosmetyki' }
-    ];
-
-    attributes.forEach(attribute => {
-      tbody += `<tr><th scope="row">${attribute.label}</th>`;
-      data.ranks.forEach(rank => {
-        let value = rank[attribute.key];
-        if (typeof value === 'boolean') {
-          value = value ? '<i class="fas fa-check"></i>' : '<i class="fas fa-times"></i>';
-        }
-        tbody += `<td>${value}</td>`;
-      });
-      tbody += '</tr>';
-    });
+    createHeaders(data, table);
+    createBody(data, table)
 
     tbody += '</tbody>';
     table.innerHTML += tbody;
